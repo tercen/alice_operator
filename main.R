45d079ad55b3cf655d8faf0ab5c588eab909c878
wptmdoorn <wptmdoorn@gmail.com>
2022-01-16 11:54:05 +0000

    Update main and workspace.R file to connect with Tercen API

--- main.R
+++ main.R
@@ -1,11 +1,20 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
+source("R/ALICE.R")
 
 ctx = tercenCtx()
 
 ctx %>%
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+  select() %>%
+  data.table() %>%
+  list %>%
+  ALICE_pipeline(
+    DTlist = .,
+    folder = "_rawdata",
+    cores = 1,
+    iter = 10,
+    nrec = 5e5
+  ) %>%
+  as.data.frame() %>%
   ctx$addNamespace() %>%
   ctx$save()
--- workspace.R
+++ workspace.R
@@ -1,8 +1,14 @@
 library(tercen)
 library(dplyr, warn.conflicts = FALSE)
+source("R/ALICE.R")
 
-options("tercen.workflowId" = "wwww")
-options("tercen.stepId"     = "dddd")
+# t=0
+# http://localhost:5402/admin/w/4268ffd55d3487c8c40b156b3f00969f/ds/abca350c-5105-406e-8515-019f4abbdf2e
+# t=15
+# http://localhost:5402/admin/w/4268ffd55d3487c8c40b156b3f00969f/ds/b4edd608-fb17-4075-8e6f-c2ff34a39495
+
+options("tercen.workflowId" = "4268ffd55d3487c8c40b156b3f00969f")
+options("tercen.stepId"     = "b4edd608-fb17-4075-8e6f-c2ff34a39495")
 
 getOption("tercen.workflowId")
 getOption("tercen.stepId")
@@ -10,8 +16,16 @@ getOption("tercen.stepId")
 ctx = tercenCtx()
 
 ctx %>%
-  select(.y, .ci, .ri) %>%
-  group_by(.ci, .ri) %>%
-  summarise(mean = mean(.y)) %>%
+  select() %>%
+  data.table() %>%
+  list %>%
+  ALICE_pipeline(
+    DTlist = .,
+    folder = "_rawdata",
+    cores = 1,
+    iter = 10,
+    nrec = 5e5
+  ) %>%
+  as.data.frame() %>%
   ctx$addNamespace() %>%
   ctx$save()
