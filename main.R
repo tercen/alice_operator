9a66467866a0a34c2afdad105cbddc7284bd39dc
wptmdoorn <wptmdoorn@gmail.com>
2022-01-20 19:14:44 +0000

    Properties * Include properties code in operator.json and .R files

--- main.R
+++ main.R
@@ -4,6 +4,54 @@ source("R/ALICE.R")
 
 ctx = tercenCtx()
 
+if (as.character(ctx$op.value('rankVar')) != "NULL") {
+  rankVar <- as.character(ctx$op.value('rankVar'))
+} else {
+  stop("rankVar not supplied")
+}
+
+if (as.character(ctx$op.value('rankVar')) != "NULL") {
+  rankVar <- as.character(ctx$op.value('rankVar'))
+} else {
+  stop("rankVar not supplied")
+}
+
+if (as.character(ctx$op.value('readCount')) != "NULL") {
+  readCount <- as.character(ctx$op.value('readCount'))
+} else {
+  stop("readCount not supplied")
+}
+
+if (as.character(ctx$op.value('readProportion')) != "NULL") {
+  readProportion <- as.character(ctx$op.value('readProportion'))
+} else {
+  stop("readProportion not supplied")
+}
+
+if (as.character(ctx$op.value('nucleotideSeq')) != "NULL") {
+  nucleotideSeq <- as.character(ctx$op.value('nucleotideSeq'))
+} else {
+  stop("nucleotideSeq not supplied")
+}
+
+if (as.character(ctx$op.value('aminoacidSeq')) != "NULL") {
+  aminoacidSeq <- as.character(ctx$op.value('aminoacidSeq'))
+} else {
+  stop("aminoacidSeq not supplied")
+}
+
+if (as.character(ctx$op.value('bestVGene')) != "NULL") {
+  bestVGene <- as.character(ctx$op.value('bestVGene'))
+} else {
+  stop("bestVGene not supplied")
+}
+
+if (as.character(ctx$op.value('bestJGene')) != "NULL") {
+  bestJGene <- as.character(ctx$op.value('bestJGene'))
+} else {
+  stop("bestJGene not supplied")
+}
+
 ctx %>%
   select() %>%
   data.table() %>%
--- workspace.R
+++ workspace.R
@@ -15,6 +15,48 @@ getOption("tercen.stepId")
 
 ctx = tercenCtx()
 
+if (as.character(ctx$op.value('rankVar')) != "NULL") {
+  rankVar <- as.character(ctx$op.value('rankVar'))
+} else {
+  stop("rankVar not supplied")
+}
+
+if (as.character(ctx$op.value('readCount')) != "NULL") {
+  readCount <- as.character(ctx$op.value('readCount'))
+} else {
+  stop("readCount not supplied")
+}
+
+if (as.character(ctx$op.value('readProportion')) != "NULL") {
+  readProportion <- as.character(ctx$op.value('readProportion'))
+} else {
+  stop("readProportion not supplied")
+}
+
+if (as.character(ctx$op.value('nucleotideSeq')) != "NULL") {
+  nucleotideSeq <- as.character(ctx$op.value('nucleotideSeq'))
+} else {
+  stop("nucleotideSeq not supplied")
+}
+
+if (as.character(ctx$op.value('aminoacidSeq')) != "NULL") {
+  aminoacidSeq <- as.character(ctx$op.value('aminoacidSeq'))
+} else {
+  stop("aminoacidSeq not supplied")
+}
+
+if (as.character(ctx$op.value('bestVGene')) != "NULL") {
+  bestVGene <- as.character(ctx$op.value('bestVGene'))
+} else {
+  stop("bestVGene not supplied")
+}
+
+if (as.character(ctx$op.value('bestJGene')) != "NULL") {
+  bestJGene <- as.character(ctx$op.value('bestJGene'))
+} else {
+  stop("bestJGene not supplied")
+}
+
 ctx %>%
   select() %>%
   data.table() %>%
