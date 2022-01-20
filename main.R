826ebe3c101d3ae4f9054b46807ce94296a767e4
wptmdoorn <wptmdoorn@gmail.com>
2022-01-20 20:23:14 +0000

    simplify properties, only include absolute necessary properties for ALICE+

--- main.R
+++ main.R
@@ -4,22 +4,10 @@ source("R/ALICE.R")
 
 ctx = tercenCtx()
 
-if (as.character(ctx$op.value('rankVar')) != "NULL") {
-  rankVar <- as.character(ctx$op.value('rankVar'))
-} else {
-  stop("rankVar not supplied")
-}
-
 if (as.character(ctx$op.value('readCount')) != "NULL") {
   readCount <- as.character(ctx$op.value('readCount'))
 } else {
   stop("readCount not supplied")
-}
-
-if (as.character(ctx$op.value('readProportion')) != "NULL") {
-  readProportion <- as.character(ctx$op.value('readProportion'))
-} else {
-  stop("readProportion not supplied")
 }
 
 if (as.character(ctx$op.value('nucleotideSeq')) != "NULL") {
@@ -51,9 +39,7 @@ df <- ctx$client$tableSchemaService$select(tableId)
 df <- as.data.frame(as_tibble(df))
 
 # rename column names
-names(df)[names(df) == rankVar] <- 'Rank'
 names(df)[names(df) == readCount] <- 'Read.count'
-names(df)[names(df) == rankVar] <- 'Read.proportion'
 names(df)[names(df) == nucleotideSeq] <- 'CDR3.nucleotide.sequence'
 names(df)[names(df) == aminoacidSeq] <- 'CDR3.amino.acid.sequence'
 names(df)[names(df) == bestVGene] <- 'bestVGene'
